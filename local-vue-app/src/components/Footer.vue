2bd8e86822c2e1827498b3cd5c9db99b59d93ab2
Julliard Macatuggal <julliardkun@gmail.com>
2025-11-19 09:31:07 +0800

    Make components
    Make the header, main, and footer into a component

---
+++ local-vue-app/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const copyright = ref('&copy; ' + new Date().getFullYear());
+</script>
+
+<template>
+    <footer>
+        <p>Copyright {{ copyright }}</p>
+    </footer>
+</template>
+
+<style scoped>
+    footer p {
+        position: fixed;
+        bottom: 0;
+        padding-bottom: 20px;
+        color: rgba(57, 46, 221, 0.548)
+    }
+</style>
---
+++ local-vue-app/src/components/Main.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { ref } from 'vue';
+    const name = ref('Julliard');
+</script>
+
+<template>
+    <main>
+        <h1>Hello, {{ name }}!</h1>
+        <p>I'm about to learn <a href="https://vuejs.org" target="_blank">Vue.js 3</a>!</p>
+    </main>
+</template>
+
+<style scoped>
+    main h1 {
+        display: block;
+    }
+
+    main a {
+        color: #42B883;
+    }
+</style>
